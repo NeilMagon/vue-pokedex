385684e43c40e214f2d230d42f87b39686151f68
NeilMagon <neil.magon97@gmail.com>
2024-09-12 16:21:10 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+  import SearchPokemon from './components/SearchPokemon.vue'
+  export default{
+    components:{
+      SearchPokemon,
+    }
+  }
+</script>
+
+<template>
+  <div class="text-center m-2">
+    <h1>PokeVue</h1>
+  </div>
+  <SearchPokemon></SearchPokemon>
+</template>
+
+<style lang="scss" scoped>
+h1{
+  font-size: 4rem;
+  font-weight: 800;
+  background: linear-gradient(to right, red, blue);
+  background-clip: text;
+  color: transparent;
+}
+</style>
---
+++ src/components/SearchPokemon.vue
@@ -0,0 +1,13 @@
+<script>
+export default{
+    name: 'SearchPokemon',
+}
+</script>
+
+<template>
+
+</template>
+
+<style lang="scss" scoped>
+
+</style>
