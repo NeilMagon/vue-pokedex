6ec06a71f1ecc0fbd71e2a8ac5579c1b70d06388
NeilMagon <neil.magon97@gmail.com>
2024-09-12 16:33:55 +0200

    create serach components and test api

--- src/components/SearchPokemon.vue
+++ src/components/SearchPokemon.vue
@@ -1,11 +1,29 @@
 <script>
-export default{
-    name: 'SearchPokemon',
-}
+    import axios from 'axios'
+    export default{
+        name: 'SearchPokemon',
+        data() {
+            return {
+                pokemonName: '',
+            }
+        },
+        methods: {
+            searchPokemon() {
+                axios.get(`https://pokeapi.co/api/v2/pokemon/${this.pokemonName.toLowerCase()}`)
+                .then(response => {
+                    console.log(response.data);
+                })
+                .catch(error => {
+                    console.error('Pokémon non trovato!', error);
+                });
+            }
+        }
+    }
 </script>
 
 <template>
-
+    <input v-model="pokemonName" placeholder="Cerca un pokemon">
+    <button @click="searchPokemon">Cerca</button>
 </template>
 
 <style lang="scss" scoped>
